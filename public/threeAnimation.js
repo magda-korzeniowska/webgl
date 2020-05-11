dd740976310c3ec5306ac4a4d9637115078886ea
magda-korzeniowska <m.korzeniowska@tlen.pl>
2020-05-11 16:48:22 +0200

    Add reversed cube

--- public/threeAnimation.js
+++ public/threeAnimation.js
@@ -3,15 +3,18 @@ import {nextSlide, prevSlide} from "../main.js"
 
 const raycaster = new THREE.Raycaster()
 const objLoader = new THREE.OBJLoader()
-let arrowBox = null
-let arrowBoxRotation = 0
+
+let arrowBoxUp = null
+let arrowBoxDown = null
+let arrowBoxUpRotation = 0
+let arrowBoxDownRotation = 0
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight)
 
 const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true})
 renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.render(scene, camera)
+// renderer.render(scene, camera)
 
 document.body.append(renderer.domElement)
 
@@ -21,19 +24,19 @@ objLoader.load(
       const screenBorderRight = visibleWidthAtZDepth(-10, camera) / 2
       const screenBottom = -visibleHeightAtZDepth(-10, camera) / 2
 
-      addCube(children[0], prevSlide, screenBorderRight - 1.5, screenBottom + 1)
-      // addCube(children[0], nextSlide, screenBorderRight - 2.5, screenBottom + 1)
+      addCube(children[0], nextSlide, screenBorderRight - 1.5, screenBottom + 1)
+      addCube(children[0], prevSlide, screenBorderRight - 2.5, screenBottom + 1, true)
 
       animate()
     }
 )
 
-const addCube = (object, callbackFn, x, y) => {
+const addCube = (object, callbackFn, x, y, reversed) => {
   const cubeMesh = object.clone()
 
   cubeMesh.scale.setScalar(.3)
-  cubeMesh.rotation.set(THREE.Math.degToRad(90), 0, 0)
-
+  reversed ? cubeMesh.rotation.set(THREE.Math.degToRad(270),0,330) : cubeMesh.rotation.set(THREE.Math.degToRad(90),0,0)
+  
   const boundingBox = new THREE.Mesh(
       new THREE.BoxGeometry(.7, .7, .7),
       new THREE.MeshBasicMaterial({transparent: true, opacity: 0})
@@ -47,20 +50,22 @@ const addCube = (object, callbackFn, x, y) => {
 
   boundingBox.callbackFn = callbackFn
 
-  arrowBox = boundingBox
+  reversed ? arrowBoxDown = boundingBox : arrowBoxUp  = boundingBox
   scene.add(boundingBox)
 }
 
 const animate = () => {
-  arrowBoxRotation = lerp(arrowBoxRotation, 0, .07)
-  arrowBox.rotation.set(THREE.Math.degToRad(arrowBoxRotation), 0, 0)
+  arrowBoxUpRotation = lerp(arrowBoxUpRotation, 0, .07)
+  arrowBoxUp.rotation.set(THREE.Math.degToRad(arrowBoxUpRotation), 0, 0)
+
+  arrowBoxDownRotation = lerp(arrowBoxDownRotation, 0, .07)
+  arrowBoxDown.rotation.set(THREE.Math.degToRad(arrowBoxDownRotation), 0, 0)
 
   renderer.render(scene, camera)
   requestAnimationFrame(animate)
 }
 
 export const handleThreeAnimation = () => {
-  arrowBoxRotation = 360
 }
 
 window.addEventListener('click', () => {
@@ -70,6 +75,14 @@ window.addEventListener('click', () => {
 
   raycaster.setFromCamera(mousePosition, camera)
 
-  const interesctedObjects = raycaster.intersectObjects([arrowBox])
-  interesctedObjects.length && interesctedObjects[0].object.callbackFn()
-}) 
+  const intersectedObjects = raycaster.intersectObjects(scene.children)
+  intersectedObjects.length && intersectedObjects[0].object.callbackFn()
+
+  if (raycaster.intersectObjects([arrowBoxUp]).length) {
+    arrowBoxUpRotation = 360
+  } else if (raycaster.intersectObjects([arrowBoxDown]).length){
+    arrowBoxDownRotation = -360
+  }
+})
+
+
