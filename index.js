19de443652033e3cfc119e53241ed35877acf6dd
magda-korzeniowska <m.korzeniowska@tlen.pl>
2020-05-12 17:17:19 +0200

    ready to deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + "/public/"));
 
@@ -8,6 +9,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.listen(8000, () => {
-  console.log('webgl-daftacademy on 8000!')
+app.listen(port, () => {
+  console.log('success! webgl-daftacademy works!')
 });
